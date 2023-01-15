3774f944095ec5b58a7d35e0631fffea2469e6a1
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2023-01-16 00:12:13 +0900

    feat: react-router

--- packages/front/src/main.tsx
+++ packages/front/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Header } from "./components/layouts/header";
+import { Home } from "./pages/home";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const BrowserRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Header />
+    <RouterProvider router={BrowserRouter} />
+  </React.StrictMode>
+);
