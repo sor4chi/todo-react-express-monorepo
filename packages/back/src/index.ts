4b26b9f21c0a440b74177376c0d2c197a09b4c09
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2023-01-16 00:26:17 +0900

    feat: linter/formatter

--- packages/back/src/index.ts
+++ packages/back/src/index.ts
@@ -1,13 +1,13 @@
-import * as express from "express";
-import { json } from "body-parser";
+import * as express from 'express';
+import { json } from 'body-parser';
 
 const app = express();
 app.use(json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000!");
+  console.log('Listening on port 3000!');
 });
--- packages/front/src/main.tsx
+++ packages/front/src/main.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Header } from "./components/layouts/header";
-import { Home } from "./pages/home";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Header } from './components/layouts/header';
+import { Home } from './pages/home';
 
 const BrowserRouter = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Home />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Header />
     <RouterProvider router={BrowserRouter} />
